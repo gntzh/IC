34c0da63735e8b8f6221e4688f89e16b64ca1581
ShoorDay <ShoorDay@users.noreply.github.com>
2020-05-13 01:46:51 +0000

    deploy: dfd6509d2e850ffd02dc0ddd31366e5e7f51eab8

--- service-worker.js
+++ service-worker.js
@@ -27,11 +27,11 @@ self.addEventListener('message', (event) => {
 self.__precacheManifest = [
   {
     "url": "404.html",
-    "revision": "6055a607597baa48677ad7972f3ac71c"
+    "revision": "d3d179721af962fbd4716c941e546923"
   },
   {
     "url": "about.html",
-    "revision": "25cdb7c0fb269bb7b5282805bea97dbd"
+    "revision": "fd6474d644ea0dbeee2fa88e1cabb5f7"
   },
   {
     "url": "assets/css/0.styles.0a437329.css",
@@ -42,24 +42,28 @@ self.__precacheManifest = [
     "revision": "83621669651b9a3d4bf64d1a670ad856"
   },
   {
-    "url": "assets/js/10.a862db66.js",
-    "revision": "fd86895965c1467f37c0109dce3f9c3b"
+    "url": "assets/js/10.092b3731.js",
+    "revision": "5a47e2bcdf09849c1c006d9f1f8cfa86"
   },
   {
-    "url": "assets/js/11.8f92b553.js",
-    "revision": "490d45d474fc03a2d100d24047797b0f"
+    "url": "assets/js/11.7054579a.js",
+    "revision": "99be45a440754a24492b0eaca652e1ca"
   },
   {
-    "url": "assets/js/12.a512bbd5.js",
-    "revision": "e78da1662a92e48f0bb3356e154e4fbc"
+    "url": "assets/js/12.fe9ba30d.js",
+    "revision": "3cfdf27b9bd25f3bd4910a013455d5a3"
+  },
+  {
+    "url": "assets/js/13.62643193.js",
+    "revision": "4ed3d02db1380fb3d63ecfaa21c0a4f5"
   },
   {
     "url": "assets/js/2.6896a865.js",
     "revision": "54531fe7421304b9af6207b59ad7ad4c"
   },
   {
-    "url": "assets/js/3.77384424.js",
-    "revision": "b969c8720a2d7ff3cd63ac543e8a46d5"
+    "url": "assets/js/3.e914d67c.js",
+    "revision": "63d79517a4e20abcbb339e62adfced16"
   },
   {
     "url": "assets/js/4.406f636c.js",
@@ -74,20 +78,20 @@ self.__precacheManifest = [
     "revision": "3820f1e0baf0899578833fcdb6c24456"
   },
   {
-    "url": "assets/js/7.0d9b7aa3.js",
-    "revision": "a7e1bd8c32f578ca4a67aeb3799e1042"
+    "url": "assets/js/7.a0ad6bce.js",
+    "revision": "746dd27483cee5b4772b7d5961e09cba"
   },
   {
-    "url": "assets/js/8.0487c79f.js",
-    "revision": "7b39d4aaac3d9d968c0193f634be596f"
+    "url": "assets/js/8.828d53c8.js",
+    "revision": "8f2b98ef27e5c9348f28468e32163601"
   },
   {
-    "url": "assets/js/9.92d70850.js",
-    "revision": "a8f4affa6700d7de7f7fd3604db82557"
+    "url": "assets/js/9.30e92171.js",
+    "revision": "81e0e942f078eb81230c6557fd6eb06c"
   },
   {
-    "url": "assets/js/app.d3091c8a.js",
-    "revision": "8f613b6198ede71e0c0ceffa943bc6c5"
+    "url": "assets/js/app.5973aebe.js",
+    "revision": "5991ae457ca089e72774cb26a326b767"
   },
   {
     "url": "favicon-192x192.png",
@@ -103,27 +107,31 @@ self.__precacheManifest = [
   },
   {
     "url": "guide/index.html",
-    "revision": "34809673ee930be73760bd8dbd31a6bc"
+    "revision": "a7ef7635d76cb928c351a37313513dce"
   },
   {
     "url": "guide/前端.html",
-    "revision": "3685c22b90e3af4bc50270340ba75252"
+    "revision": "e0c99779701f0f0eb2a531d2f655c4e1"
+  },
+  {
+    "url": "guide/功能需求.html",
+    "revision": "7bda21d803c8139445956783472ea26c"
   },
   {
     "url": "guide/后端.html",
-    "revision": "8641cc2860d003abe6e47d8f14f25cf6"
+    "revision": "c063ef92d0f5480bf922661ed0ab0d33"
   },
   {
     "url": "guide/部署.html",
-    "revision": "8686e619ab41bc4014f82a05c346927f"
+    "revision": "8e4658205e64c44991f6c7deb3c7a056"
   },
   {
     "url": "guide/部署参考.html",
-    "revision": "d13708c1d44074a85e7d598acb8c3157"
+    "revision": "130a9b07a986aeb2125f7b6f71c081d2"
   },
   {
     "url": "index.html",
-    "revision": "f16fc7d51e881ad1716b8093233a85bb"
+    "revision": "7111e9af52584ff12d5b93f8ff06c2d8"
   }
 ].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
